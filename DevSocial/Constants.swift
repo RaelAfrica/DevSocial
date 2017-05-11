b363c45f13f000ce3994df7195ed6982d3c8bd0d
Rael Kenny <RaelKenny@gmail.com>
2017-05-11 12:38:46 +0200

    sign in UI custom classes

---
+++ DevSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DevSocial
+//
+//  Created by Rael Kenny on 5/11/17.
+//  Copyright © 2017 Rael Kenny. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ DevSocial/FancyButton.swift
@@ -0,0 +1,27 @@
+//
+//  FancyButton.swift
+//  DevSocial
+//
+//  Created by Rael Kenny on 5/11/17.
+//  Copyright © 2017 Rael Kenny. All rights reserved.
+//
+
+import UIKit
+
+class FancyButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+        
+    }
+    
+
+    
+    
+}
